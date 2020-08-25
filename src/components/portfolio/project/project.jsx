c73b6e90641e084d029646690753b8ed95089203
jsimpson1426 <simpsonjoshuam@gmail.com>
2020-08-25 17:52:58 -0400

    CANNOT Display: Inline Block. Edited style on Project component

--- src/components/portfolio/project/project.jsx
+++ src/components/portfolio/project/project.jsx
@@ -10,22 +10,26 @@ class Project extends Component {
         <div className="project-div-bg2">
           <div className="project-div-container">
             <h2 className="project-h2">{content.title}</h2>
-            <img
-              className="project-img"
-              alt={content.alt}
-              src={content.imgUrl}
-            />
-            <div className="project-div-right">
-              <p className="project-p">{content.description}</p>
-              <div className="project-div-buttons">
-                <a className="project-a" href={content.projectUrl}>
-                  View Project
-                </a>
-                <a className="project-a" href={content.githubUrl}>
-                  GitHub
-                </a>
-              </div>
-            </div>
+            <ul className="project-ul">
+              <li className="project-li-left">
+                <img
+                  className="project-img"
+                  alt={content.alt}
+                  src={content.imgUrl}
+                />
+              </li>
+              <li className="project-li-right">
+                <p className="project-p">{content.description}</p>
+                <div className="project-div-buttons">
+                  <a className="project-a" href={content.projectUrl}>
+                    View Project
+                  </a>
+                  <a className="project-a" href={content.githubUrl}>
+                    GitHub
+                  </a>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
